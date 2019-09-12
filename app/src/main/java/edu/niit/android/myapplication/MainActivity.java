0c8869c6b2b75f01c55ad7f30690492bcc4dc6bf
kaixin1013 <2413890705@qq.com>
2019-09-12 14:12:29 +0800

    “

--- app/src/main/java/edu/niit/android/myapplication/MainActivity.java
+++ app/src/main/java/edu/niit/android/myapplication/MainActivity.java
@@ -1,7 +1,12 @@
 package edu.niit.android.myapplication;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.SparseArray;
+import android.widget.Space;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initTitles();
+        initFragment();
     }
+
+    private void initTitles() {
+    }
+
+    private void initFragment() {
+        //1.创建Fragment的列表
+        fragments=new SparseArray<>();
+        fragments.put(R.id.btn_my,MySettingFragment.newInstance());
+        //2.加载默认的fragment
+        replaceFragment(fragments.get(R.id.btn_my));
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager manager=getSupportFragmentManager();
+        FragmentTransaction ft=manager.beginTransaction();
+        ft.replace(R.id.main_body,fragment);
+        ft.commit();
+    }
+
+    private SparseArray<Fragment> fragments;
+
 }
---
+++ app/src/main/java/edu/niit/android/myapplication/SettingActivity.java
@@ -0,0 +1,19 @@
+package edu.niit.android.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import edu.niit.android.myapplication.utils.StatusUtils;
+
+
+public class SettingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        StatusUtils.setImmersionMode(this);
+        setContentView(R.layout.activity_setting);
+
+        StatusUtils.initToolbar(this, "设置", true, false);
+    }
+}
